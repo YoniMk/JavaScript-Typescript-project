4c7e86cf86fa85b51f1f285837bf18cc885f48e6
YoniMk <149786960+YoniMk@users.noreply.github.com>
2024-07-02 23:41:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,13 @@
-// script.js
-document.getElementById('calculatorForm').addEventListener('submit', function(event){
-    event.preventDefault(); // Prevent the default form submition
+document.getElementById("loginForm").addEventListener("submit", function(event) { event.preventDefault()
 
-    //Get the input values
-    var initialInvestment = parseFloat(document.getElementById('initialInvestment').value);
-    var monthlyContribution = parseFloat(document.getElementById('monthlyContribution').value);
-    var annualInterestRate = parseFloat(document.getElementById('annualInterestRate').value) / 100; // Convert percentage
-    var investmentDuration = parseFloat(document.getElementById('investmentDuration').value);
+var userName = document.getElementById("userName").value.trim();
+var password = document.getElementById("password").value.trim();
 
-    // Check if the input values are valid numbers
-    if (!isNaN(initialInvestment) && !isNaN(monthlyContribution) && !isNaN(annualInterestRate) && !isNaN(investmentDuration)){
-        // Calculate the future value using compound interest formula
-        var n = 12; // compounded monthly
-        var t = investmentDuration; //Time in years
-        var futureValue = initialInvestment * Math.pow(1+ (annualInterestRate / n), n * t) + (monthlyContribution * ((Math.pow(1 +(annualInterestRate / n), n* t) - 1) / (annualInterestRate / n )));
 
-        // Display the future value on the web page
-        document.getElementById('futureValue').textContent = "Your future value after" + investmentDuration + "year is: $" + futureValue.toFixed(2);
-    } else {
-        // Display an error message for invalid inputs
-        document.getElementById('futureValue').textContent = "Please enter valid number for all fields.";
+if((userName === "admin" && password ==="12345") || (userName === "user123" && password === "123")) {
+    alert("Login Successfull")
+ }
+  else {
+        alert("The password or username doesn't match.Please try again!");
     }
-
-    // Reset the form
-    document.getElementById('calculatorForm').reset();
-
 });
